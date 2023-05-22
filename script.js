7c835f2cc47afa17f5957d84b379862cfa9354ff
Lucas Da Silva Garcia <lsgarcia.306@gmail.com>
2023-05-21 21:18:20 -0400

    Iniciando  javaScript para tratar os dados do form

---
+++ script.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector("#btnEnviar");
+
+btn.addEventListener("click", function(event) {
+
+event.preventDefault();
+
+const nome = document.querySelector("#inputNome").value
+const email = document.querySelector("#inputEmail").value
+const estado = document.querySelector("#inputEstado").value
+const cidade = document.querySelector("#inputCidade").value
+const msgUsuario = document.querySelector("#comentario").value
+
+
+})
+
+
+
