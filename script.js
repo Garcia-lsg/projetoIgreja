88d1f70ae9b914ccc7bbf5b27718ea9b6facf556
Lucas Garcia <lsgarcia.306@gmail.com>
2023-05-23 21:05:33 -0400

    Atualização
    Relização da responsividade e envio de email.

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const btn = document.querySelector("#btnEnviar");
+// const btn = document.querySelector("#btnEnviar");
 
-btn.addEventListener("click", function(event) {
+// btn.addEventListener("click", function(event) {
 
-event.preventDefault();
+// event.preventDefault();
 
-const nome = document.querySelector("#inputNome").value
-const email = document.querySelector("#inputEmail").value
-const estado = document.querySelector("#inputEstado").value
-const cidade = document.querySelector("#inputCidade").value
-const msgUsuario = document.querySelector("#comentario").value
+// const nome = document.querySelector("#inputNome").value
+// const email = document.querySelector("#inputEmail").value
+// const estado = document.querySelector("#inputEstado").value
+// const cidade = document.querySelector("#inputCidade").value
+// const msgUsuario = document.querySelector("#comentario").value
 
+// console.log(nome)
 
-})
-
+// })
 
 
